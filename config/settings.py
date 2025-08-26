a0d0d8084e31949bad8bae4e5ba9813d744b8ab9
ivan_kalinov <ikalinov1989@yandex.ru>
2025-08-26 19:31:44 +0400

    Add validators

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "rest_framework_simplejwt",
+    "corsheaders",
 
     "users",
     "habits",
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.db import router
 from django.urls import path
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import UserViewSet
 
@@ -10,6 +13,7 @@ router = SimpleRouter()
 router.register("", UserViewSet, basename="users")
 
 urlpatterns = [
-
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)),name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh")
 ]
 urlpatterns += router.urls
